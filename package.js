4efcd18206816b7b4f6b8956c79365dedf210a6b
Sébastien Vidal <Sbastien Vidal>
2016-06-16 16:32:32 +0200

    ajout des tests & des packages

---
+++ package.js
@@ -0,0 +1,32 @@
+{
+  "name": "Date-Prototypes",
+  "version": "0.0.1",
+  "description": "Increase the Date object's potential",
+  "main": "lib/Date-Prototypes.js",
+  "scripts": {
+    "start": "node lib/Date-Prototypes.js start",
+    "test": "gulp"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git://github.com/Psychopoulet/Date-Prototypes"
+  },
+  "keywords": [
+    "date",
+    "formate",
+    "parse"
+  ],
+  "author": "Sébastien VIDAL",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/Psychopoulet/Date-Prototypes/issues"
+  },
+  "dependencies": {},
+  "devDependencies": {
+    "gulp": "^3.9.0",
+    "gulp-eslint": "^2.0.0",
+    "gulp-exclude-gitignore": "^1.0.0",
+    "gulp-plumber": "^1.0.0"
+  },
+  "homepage": "https://github.com/Psychopoulet/Date-Prototypes#readme"
+}
